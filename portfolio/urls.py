d2597b9dc9b7ea9dcc30f4b5f79acf0ebd08e9fa
Pruthviraj Rathod <125789327+pruthvi4344@users.noreply.github.com>
2025-11-09 22:34:29 +0530

    Remove static and media file serving condition
    Removed conditional serving of media and static files in development.

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -7,8 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# ✅ Serve media and static files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
